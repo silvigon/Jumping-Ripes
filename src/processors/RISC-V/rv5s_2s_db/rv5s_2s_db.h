d2e3b5539e1da18f91c116af2117d787a36bf5cd
Silvia González Rodríguez <silvia.gonzalez17@alu.uclm.es>
2025-02-28 22:59:46 +0000

    Fix clearing ID-EX register on exit syscall with delayed branches

--- src/processors/RISC-V/rv5s_2s_db/rv5s_2s_db.h
+++ src/processors/RISC-V/rv5s_2s_db/rv5s_2s_db.h
@@ -44,7 +44,8 @@ class RV5S_2S_DB : public RipesVSRTLProcessor {
 public:
   enum Stage { IF = 0, ID = 1, EX = 2, MEM = 3, WB = 4, STAGECOUNT };
   RV5S_2S_DB(const QStringList &extensions)
-      : RipesVSRTLProcessor("5-Stage RISC-V Processor (2-slot delayed branch)") {
+      : RipesVSRTLProcessor(
+            "5-Stage RISC-V Processor (2-slot delayed branch)") {
     m_enabledISA = ISAInfoRegistry::getISA<XLenToRVISA<XLEN>()>(extensions);
     decode->setISA(m_enabledISA);
     uncompress->setISA(m_enabledISA);
@@ -66,15 +67,12 @@ public:
     // boolean 0/1 values.
     controlflow_or->out >> pc_src->select;
 
-    // MODIFIED: routes syscallExit straight to ifid_reg.clear
-    //controlflow_or->out >> *efsc_or->in[0];
-    //ecallChecker->syscallExit >> *efsc_or->in[1];
-    ecallChecker->syscallExit >> ifid_reg->clear;
-
-    // MODIFIED: routes hazardIDEXClear straight to idex_reg.clear
-    //efsc_or->out >> *efschz_or->in[0];
-    //hzunit->hazardIDEXClear >> *efschz_or->in[1];
-    hzunit->hazardIDEXClear >> idex_reg->clear;
+    // MODIFIED: disconnect controlflow and syscallExit signals from efsc_or
+    // controlflow_or->out >> *efsc_or->in[0];
+    // ecallChecker->syscallExit >> *efsc_or->in[1];
+    // efsc_or->out >> *efschz_or->in[0];
+    ecallChecker->syscallExit >> *efschz_or->in[0];
+    hzunit->hazardIDEXClear >> *efschz_or->in[1];
 
     // -----------------------------------------------------------------------
     // Instruction memory
@@ -175,7 +173,9 @@ public:
     pc_reg->out >> ifid_reg->pc_in;
     uncompress->exp_instr >> ifid_reg->instr_in;
     hzunit->hazardFEEnable >> ifid_reg->enable;
-    //efsc_or->out >> ifid_reg->clear;		// MODIFIED
+    // MODIFIED: IF/ID is cleared directly by syscallExit
+    // efsc_or->out >> ifid_reg->clear;
+    ecallChecker->syscallExit >> ifid_reg->clear;
     1 >> ifid_reg->valid_in; // Always valid unless register is cleared
 
     // -----------------------------------------------------------------------
@@ -186,7 +186,7 @@ public:
     // ID/EX
     hzunit->hazardIDEXEnable >> idex_reg->enable;
     hzunit->hazardIDEXClear >> idex_reg->stalled_in;
-    //efschz_or->out >> idex_reg->clear;	// MODIFIED
+    efschz_or->out >> idex_reg->clear;
 
     // Data
     ifid_reg->pc4_out >> idex_reg->pc4_in;
@@ -324,9 +324,9 @@ public:
   // True if branch taken or jump instruction
   SUBCOMPONENT(controlflow_or, TYPE(Or<1, 2>));
   // True if controlflow action or performing syscall finishing
-  //SUBCOMPONENT(efsc_or, TYPE(Or<1, 2>));		// MODIFIED
+  // SUBCOMPONENT(efsc_or, TYPE(Or<1, 2>));		// MODIFIED
   // True if above or stalling due to load-use hazard
-  //SUBCOMPONENT(efschz_or, TYPE(Or<1, 2>));	// MODIFIED
+  SUBCOMPONENT(efschz_or, TYPE(Or<1, 2>));
 
   SUBCOMPONENT(mem_stalled_or, TYPE(Or<1, 2>));
 
